0ac54b4ce1e8f2d63e260059645d2cb3bb71d14a
jungwon11082015 <79095163+jungwon11082015@users.noreply.github.com>
2021-11-19 15:18:43 +0900

    프로젝트 세팅중
    프로젝트 세팅중

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+     <Header/>
+    <router-view/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/_reset.scss';  
+#app { 
+  margin:0; padding:0;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center; 
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <p>메인컴포넌트</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
